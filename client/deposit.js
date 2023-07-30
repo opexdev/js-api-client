f93b5bd0ba5308478d597e1d8f8f02abdaba4a35
Hossein <hossein13785ab@gmail.com>
2023-07-30 17:08:17 +0330

    Add network to deposit api

--- client/deposit.js
+++ client/deposit.js
@@ -1,9 +1,10 @@
 import axios from "axios";
 
-export const getDepositAddress = (currency, timestamp = Date.now().toString()) => {
+export const getDepositAddress = (currency, timestamp = Date.now().toString(), network) => {
     const params = new URLSearchParams();
     params.append('coin', currency.toUpperCase());
     params.append('timestamp', timestamp);
+    params.append('network', network);
     return axios.get(`/sapi/v1/capital/deposit/address?${params.toString()}`, {
         data: params,
         headers: {
