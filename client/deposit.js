c185b3b2b61c2655592b4d31276473bed5b47a25
Hossein <hossein13785ab@gmail.com>
2023-07-30 17:16:44 +0330

    Fix network name in deposit api

--- client/deposit.js
+++ client/deposit.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
-export const getDepositAddress = (currency, timestamp = Date.now().toString(), network) => {
+export const getDepositAddress = (currency, network, timestamp = Date.now().toString()) => {
     const params = new URLSearchParams();
     params.append('coin', currency.toUpperCase());
-    params.append('timestamp', timestamp);
     params.append('network', network);
+    params.append('timestamp', timestamp);
     return axios.get(`/sapi/v1/capital/deposit/address?${params.toString()}`, {
         data: params,
         headers: {
