217ac93076c92fcedc157de8c949a9fdf7e9829d
Hossein <hossein13785ab@gmail.com>
2024-09-09 16:14:21 +0330

    #18: Add request body for WithdrawReq

--- client/withdraw.js
+++ client/withdraw.js
@@ -1,17 +1,5 @@
 import axios from "axios";
 
-export const sendWithdrawReq = async (amount, currency, address, fee, network) => {
-    const params = new URLSearchParams();
-    params.append('fee', fee);
-    params.append('destSymbol', currency.toUpperCase());
-    params.append('destAddress', address);
-    params.append('destNetwork', network);
-    return axios.post(`/wallet/withdraw/${amount}_${currency.toUpperCase()}`, null,
-        {
-            params,
-            headers: {
-                "Content-Type": 'application/x-www-form-urlencoded'
-            }
-        }
-    )
+export const sendWithdrawReq = async (withdrawRequestData) => {
+    return axios.post(`/wallet/withdraw`, withdrawRequestData)
 }
