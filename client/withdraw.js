94056f6ab521367865593a0a2f0f7e64f3e939ea
Hossein <hossein13785ab@gmail.com>
2024-09-24 13:31:31 +0330

    #19: Fix cancelWithdrawReq api

--- client/withdraw.js
+++ client/withdraw.js
@@ -4,6 +4,6 @@ export const sendWithdrawReq = async (withdrawRequestData) => {
     return axios.post(`/wallet/withdraw`, withdrawRequestData)
 }
 
-export const cancelWithdrawReq = async () => {
+export const cancelWithdrawReq = async (withdrawId) => {
     return axios.post(`/wallet/withdraw/${withdrawId}/cancel`)
 }
