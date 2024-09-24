e5090871fcbba4ef0a826f2028e0c67a7574fd02
Hossein <hossein13785ab@gmail.com>
2024-09-24 12:52:32 +0330

    #19: Add cancelWithdrawReq api

--- client/withdraw.js
+++ client/withdraw.js
@@ -3,3 +3,7 @@ import axios from "axios";
 export const sendWithdrawReq = async (withdrawRequestData) => {
     return axios.post(`/wallet/withdraw`, withdrawRequestData)
 }
+
+export const cancelWithdrawReq = async () => {
+    return axios.post(`/wallet/withdraw/${withdrawId}/cancel`)
+}
