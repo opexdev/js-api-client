bcb951f3220a482b4262d603eef9c688d5b31a37
Benyamin <Koorosh.azimi1@gmail.com>
2024-09-24 15:32:43 +0330

    #19: Update Withdraw APIs

--- admin/withdraw.js
+++ admin/withdraw.js
@@ -4,26 +4,24 @@ export const adminGetWithdrawsReq = (status, page, perPage) => {
     const params = {
         offset: perPage * (page - 1),
         size: perPage,
-        ...(status && {status})
     }
-    return axios.get('/wallet/admin/withdraw', {params})
-}
-export const adminGetWithdrawReqById = (withdraw_id) => {
-    return axios.get("/wallet/admin/withdraw/", {
-        params: {
-            withdraw_id,
-            offset: 0,
-            size: 1
-        }
+    return axios.post('/wallet/admin/withdraw/search', {
+        "status": [status]
+    },{
+        params
     })
 }
+
+export const adminGetWithdrawReqById = (withdraw_id) => {
+    return axios.get(`/wallet/admin/withdraw/${withdraw_id}`,)
+}
+
 export const adminSetWithdrawStatus = (id, withdrawState, withdrawExp) => {
     const params = new URLSearchParams();
     if (withdrawState === "accept") {
-        params.append('fee', "0");
         params.append('destTransactionRef', withdrawExp);
     } else {
-        params.append('statusReason', withdrawExp);
+        params.append('reason', withdrawExp);
     }
     return axios.post(`/wallet/admin/withdraw/${id}/${withdrawState}`, params, {
         headers: {'Content-Type': "application/x-www-form-urlencoded"},
