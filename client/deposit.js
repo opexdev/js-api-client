e3bb06671718e426e03d618c6a742e079c6a1449
Hossein-ab99 <hossein13785ab@gmail.com>
2025-02-14 18:38:45 +0330

    fix: update parameters for getDepositAddress service

--- client/deposit.js
+++ client/deposit.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export const getDepositAddress = (currency, network, timestamp = Date.now().toString()) => {
     const params = new URLSearchParams();
     params.append('coin', currency.toUpperCase());
-    params.append('chain', network);
+    params.append('network', network);
     params.append('timestamp', timestamp);
     return axios.get(`/sapi/v1/capital/deposit/address?${params.toString()}`, {
         data: params,
