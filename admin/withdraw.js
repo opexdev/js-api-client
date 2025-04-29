2e66ac501fa5b33006c84d2d61cde88784f28fd3
Hossein <hossein13785ab@gmail.com>
2025-04-29 17:41:08 +0330

    refactor: use raw JSON body in adminSetWithdrawStatus API

--- admin/withdraw.js
+++ admin/withdraw.js
@@ -16,15 +16,17 @@ export const adminGetWithdrawReqById = (withdraw_id) => {
     return axios.get(`/wallet/admin/withdraw/${withdraw_id}`,)
 }
 
-export const adminSetWithdrawStatus = (id, withdrawState, withdrawExp, destNote=null) => {
-    const params = new URLSearchParams();
+export const adminSetWithdrawStatus = (id, withdrawState, withdrawExp, destNote = null) => {
+    let data = {};
+
     if (withdrawState === "accept") {
-        params.append('destTransactionRef', withdrawExp);
-        params.append('destNote', destNote);
+        data.destTransactionRef = withdrawExp;
+        data.destNote = destNote;
     } else {
-        params.append('reason', withdrawExp);
+        data.reason = withdrawExp;
     }
-    return axios.post(`/wallet/admin/withdraw/${id}/${withdrawState}`, params, {
-        headers: {'Content-Type': "application/x-www-form-urlencoded"},
-    })
-}
+
+    return axios.post(`/wallet/admin/withdraw/${id}/${withdrawState}`, data, {
+        headers: { 'Content-Type': 'application/json' }
+    });
+};
